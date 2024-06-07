1311f3a3f3d32e4774081b921e6f03388369af79
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-06-07 22:05:10 +0700

    Clean code
    organized code, cleaning it by remove unnecessary
    line, group them by their purpose and then add
    comments, and fixing some styling
    add js folder for javascript file, and remove
    script tag from all html file

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.getElementById('aboutButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    const aboutBox = document.getElementById('about');
+    aboutBox.classList.add('show');
+
+    setTimeout(() => {
+      aboutBox.classList.add('scale');
+    }, 500);
+});
+
+document.getElementById('backAboutButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('about').classList.remove('show');
+    setTimeout(() => {
+      document.getElementById('about').classList.remove('scale');
+    }, 500);
+});
+
+document.addEventListener('click', function(event) {
+    var checkbox = document.getElementById('myCheckbox');
+    var searchBtnDiv = document.getElementById('search-btn');
+
+    if (checkbox && searchBtnDiv) {
+        var isClickInside = searchBtnDiv.contains(event.target);
+        if (!isClickInside) {
+            checkbox.checked = false;
+        }
+    }
+});
